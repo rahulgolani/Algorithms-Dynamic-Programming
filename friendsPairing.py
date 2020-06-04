c8b255b4de9971489881f78a8dd815be20cafea3
Rahul Golani <rahulgolani24@live.com>
2020-06-04 19:41:00 +0530

    10

--- friendsPairing.py
+++ friendsPairing.py
@@ -1,3 +1,4 @@
+'''
 def friendPairing(n):
     dp=[0 for i in range(n+1)]
     for i in range(2):
@@ -8,3 +9,20 @@ def friendPairing(n):
 
 if __name__ == '__main__':
     print(friendPairing(4))
+'''
+
+def countFriendsPairings(n):
+    a, b, c = 1, 1, 0;
+    if n==0:
+        return a
+    if n==1:
+        return b
+    for i in range(2, n + 1):
+        c = b + (i - 1) * a;
+        a = b;
+        b = c;
+    return c;
+
+# Driver code
+n = 3;
+print(countFriendsPairings(n));
