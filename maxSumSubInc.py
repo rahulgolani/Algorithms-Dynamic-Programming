8cc81a47ff442124f1eeacd7b7201497b4beeb3b
Rahul Golani <rahulgolani24@live.com>
2020-08-21 21:48:23 +0530

    38

--- maxSumSubInc.py
+++ maxSumSubInc.py
@@ -1,12 +1,14 @@
 def maxSumSub(arr,n):
     result=[arr[i] for i in range(n)]
+    maxResult=float('-infinity')
     for i in range(1,n):
         for j in range(i):
             if arr[j]<arr[i]:
                 result[i]=max(result[i],result[j]+arr[i])
-                print(result)
+                if result[i]>maxResult:
+                    maxResult=result[i]
 
-    return result
+    return maxResult
 
 if __name__ == '__main__':
     arr=[4,6,1,3,8,4,6]
