036ee660eb7ce12bf62450bb901725f4eb33ab2e
Rahul Golani <rahulgolani24@live.com>
2020-08-22 18:46:11 +0530

    41

---
+++ MaxSumSubsequenceNoElemAdj.py
@@ -0,0 +1,21 @@
+def Subsequence(arr):
+    incl=arr[0]
+    excl=0
+    # print(incl)
+    # print(excl)
+    # print('\n')
+    for i in range(1,len(arr)):
+        #new_excl=max(incl,excl)
+        new_incl=excl+arr[i]
+        excl=max(incl,excl)
+        incl=new_incl
+        #excl=new_excl
+        # print(incl)
+        # print(excl)
+        # print("\n")
+
+    return max(incl,excl)
+if __name__ == '__main__':
+    arr=[5,5,10,100,10,5]
+    result=Subsequence(arr)
+    print(result)
