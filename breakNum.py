841848e0c4b3598274ee908c226a0d013bd71eaa
Rahul Golani <rahulgolani24@live.com>
2020-08-29 22:13:53 +0530

    56

--- breakNum.py
+++ breakNum.py
@@ -1,7 +1,7 @@
 # Recursive Solution
 def breakSum(n):
-    if n<=0:
-        return 0
+    if n<=1:
+        return n
     return max(n,breakSum(n//2)+breakSum(n//3)+breakSum(n//4))
 
 if __name__ == '__main__':
